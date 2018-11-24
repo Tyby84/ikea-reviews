2b10437c788607cd89e119ff098304cda0772800
Tyby <tibofenyvesi84@gmail.com>
2018-11-24 23:03:54 +0000

    Responsive features..

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ $.getJSON("reviews.json", function (data) {
 				<h3 class="reviews__item__name">${name}
 				<span class="reviews__item__age">(${age})</span>
 				</h3>
-				<p class="reviews__item__product">Bought <a class="product-link" href="#">${product}</a> at IKEA</p>
+				<p class="reviews__item__product">Bought <a class="product-link" href="#">${product}</a></p>
 				<p class="reviews__item__review">${review}</p>
 				<p>${ratings == 4  ? '<img class="stars" src="stars/4stars.jpg" alt="Rating">' : ''}
 				   ${ratings == 5  ? '<img class="stars" src="stars/5stars.jpg" alt="Rating">' : ''}
