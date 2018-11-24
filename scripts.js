8b0409a67ff7f5c68a0ff6ff2c048328d904bea8
Tyby <tibofenyvesi84@gmail.com>
2018-11-24 17:12:42 +0000

    Ratings feature added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
 $.getJSON("reviews.json", function (data) {
   $.each(data, function (key, val) {
     let name = val.customer_name;
@@ -6,6 +5,8 @@ $.getJSON("reviews.json", function (data) {
     let product = val.product_name;
     let review = val.customer_review;
     let picture = val.customer_pic;
+	let ratings = val.rating;
+	  //console.log(ratings);
     $('.reviews').append(`<div class="reviews__item">
 				
 				<h3 class="reviews__item__name">${name}
@@ -14,8 +15,10 @@ $.getJSON("reviews.json", function (data) {
 				<hr class="hr">
 				<p class="reviews__item__product">Bought <a class="product-link" href="#">${product}</a> at IKEA</p>
 				<p class="reviews__item__review">${review}</p>
+				<p>${ratings == 4  ? '<img class="stars" src="stars/4stars.jpg" alt="Rating">' : ''}
+				   ${ratings == 5  ? '<img class="stars" src="stars/5stars.jpg" alt="Rating">' : ''}
+				</p>
 				</div>
 				`);
   });
-});
 });
