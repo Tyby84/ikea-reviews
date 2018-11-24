de8b05130dca40e4609b466fabcdce0f724c2e3f
Tyby <tibofenyvesi84@gmail.com>
2018-11-24 19:44:48 +0000

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,35 @@ let gulp = require('gulp'),
 	nested = require('postcss-nested'),
 	cssImport = require('postcss-import'),
 	mixins = require('postcss-mixins'),
-	hexrgba = require('postcss-hexrgba');
+	hexrgba = require('postcss-hexrgba'),
+	colorFunc = require('postcss-color-function'),
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  /*browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "IKEA"
+    }
+ });
+	
+watch('./app/index.html', function() {
+			browserSync.reload();
+		});*/
+watch('styles/*.css', function() {
+    gulp.start('styles');
+});
+	
 
 
+	
+});
+	
 gulp.task('styles', function() {
   return gulp.src('./styles/style.css')
-    	.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba ,autoprefixer]))
+    	.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba ,autoprefixer, colorFunc]))
   		.on('error', function(errorInfo) {
 	  	console.log(errorInfo.toString());
   		this.emit('end');
